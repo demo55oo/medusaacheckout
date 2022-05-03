328e9bf152b3d2a938e26d8812af3dd6bc914854
ASUS <adhammansour886@gmail.com>
2022-05-03 13:42:25 +0200

    make it better

--- medusa-config.js
+++ medusa-config.js
@@ -1,21 +1,21 @@
 // CORS when consuming Medusa from admin
+
 const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
 // Database URL (here we use a local database called medusa-development)
-const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-store";
+const DATABASE_URL =process.env.DATABASE_URL || "postgres://localhost/medusa-store";
 
 // Medusa uses Redis, so this needs configuration as well
 const REDIS_URL = process.env.REDIS_URL || "redis-17879.c240.us-east-1-3.ec2.cloud.redislabs.com:17879";
 
 // Stripe keys
-const STRIPE_API_KEY = process.env.STRIPE_API_KEY || "";
+const STRIPE_API_KEY = process.env.STRIPE_API_KEY ||"";
 const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET || "";
 
-// This is the place to include plugins. See API documentation for a thorough guide on plugins.
+// This is the place to include plugins. See API documentation for
 const plugins = [
   `medusa-fulfillment-manual`,
   `medusa-payment-manual`,
@@ -31,7 +31,7 @@ const plugins = [
 ];
 
 module.exports = {
-  projectConfig: {
+   projectConfig: {
     redis_url: REDIS_URL,
     database_url: DATABASE_URL,
     database_type: "postgres",
